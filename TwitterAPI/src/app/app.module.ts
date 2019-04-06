196037b6a77055df83447a8c030c4bc040144ae0
Erik Remington Lee <eriklee19@gmail.com>
2019-04-05 23:22:14 -0400

    Website components created

--- TwitterAPI/src/app/app.module.ts
+++ TwitterAPI/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { RandomTweetComponent } from './random-tweet/random-tweet.component';
+import { TwitterSearchComponent } from './twitter-search/twitter-search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    RandomTweetComponent,
+    TwitterSearchComponent
   ],
   imports: [
     BrowserModule,
---
+++ TwitterAPI/src/app/twitter-search/twitter-search.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-twitter-search',
+  templateUrl: './twitter-search.component.html',
+  styleUrls: ['./twitter-search.component.scss']
+})
+export class TwitterSearchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
