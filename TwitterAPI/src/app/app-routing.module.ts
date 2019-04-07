f41dea6aa94b80f9358e4a68e6014139c3545eb2
Erik Remington Lee <eriklee19@gmail.com>
2019-04-06 21:53:10 -0400

    Creating skeleton page

--- TwitterAPI/src/app/app-routing.module.ts
+++ TwitterAPI/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RandomTweetComponent } from './random-tweet/random-tweet.component';
+import { TwitterSearchComponent } from './twitter-search/twitter-search.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent},
+  { path: 'TwitterSearch', component: TwitterSearchComponent},
+  { path: 'RandomTweet', component: RandomTweetComponent},  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- TwitterAPI/src/app/app.module.ts
+++ TwitterAPI/src/app/app.module.ts
@@ -1,22 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { RandomTweetComponent } from './random-tweet/random-tweet.component';
 import { TwitterSearchComponent } from './twitter-search/twitter-search.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     RandomTweetComponent,
-    TwitterSearchComponent
+    TwitterSearchComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
