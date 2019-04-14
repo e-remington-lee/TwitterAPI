c1c14d4ab44a82d6d5d2c8408778c9c542eb386a
Erik Remington Lee <eriklee19@gmail.com>
2019-04-14 19:41:30 -0400

    getting tweets back

--- TwitterAPI/src/app/getTweets.js
+++ TwitterAPI/src/app/getTweets.js
@@ -1,3 +1,23 @@
-var access = require("./config.js")
+var access = require("./config.js");
 
-console.log(access)
+var Twit = require('Twit');
+var T = new Twit(access);
+
+var searchData = {
+    q: 'raindbow until:2019-4-10',
+    count: 10
+};
+
+
+T.get('search/tweets', searchData, getData);
+
+function getData(err, data, response) {
+    var tweet = data.statuses
+    var x = 1
+
+    for (let i = 0; i<tweet.length; i++){
+        console.log(tweet[i].text)
+        x++
+        console.log(x)
+    }
+};
