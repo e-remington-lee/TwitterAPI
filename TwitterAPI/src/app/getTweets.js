ff358e787a46bf03902eb61eee13b3b54feff5e1
Erik Remington Lee <eriklee19@gmail.com>
2019-04-15 20:31:17 -0400

    selecting a random tweet from a user

--- TwitterAPI/src/app/getTweets.js
+++ TwitterAPI/src/app/getTweets.js
@@ -4,12 +4,12 @@ var Twit = require('Twit');
 var T = new Twit(access);
 
 var searchData1 = {
-    q: 'raindbow until:2019-4-10',
+    q: 'raindbow since:2019-1-1',
     count: 10
 };
 
 
-T.get('search/tweets', searchData1, getData);
+// T.get('search/tweets', searchData1, getData);
 
 
 
@@ -17,21 +17,37 @@ function getData(err, data, response) {
     var tweet = data.statuses
     var x = 1
 
-    for (let i = 0; i<tweet.length; i++){
-        console.log(tweet[i].text)
-        x++
-        console.log(x)
-    }
+    tweet.forEach(function(item){
+        console.log(item.text);
+        x++;
+        console.log(x);
+    });
 };
 
 
-// var serachData2 = {
-//     screen_name: 'elonmusk',
-//     count: 10
-// };
+var serachData2 = {
+    screen_name: 'elonmusk',
+    count: 100,
+    include_rts: false,
+    exclude_replies: true,
+    trim_user: true
+};
 
-// T.get('users/:screenname', serachData2, getData2);
 
-// function getData2 (err,data,response) {
-//     console.log(data)
-// };
+T.get('statuses/user_timeline', serachData2, getData2);
+
+function getData2 (err,data,response) {
+
+    var userTweet = data;
+    var listTweets = [];
+    var min = 0;
+    userTweet.forEach(function(item){
+        listTweets.push(item.text);
+    });
+
+    var length = listTweets.length;
+    console.log(length);
+    var randomSelection = Math.floor(Math.random()*(length+1));
+
+    console.log(listTweets[randomSelection]);
+};
