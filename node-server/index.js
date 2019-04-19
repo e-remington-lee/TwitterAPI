f49cacd7660f35982c21b51cb2fd123f4137d3c9
Erik Remington Lee <eriklee19@gmail.com>
2019-04-18 21:34:54 -0400

    calling an internal API!

--- node-server/index.js
+++ node-server/index.js
@@ -7,6 +7,16 @@ var server = app.listen(3003, () => {
 
 app.use(express.static(__dirname+'/dist'));
 
+app.get('/api/bob', (req, res) => {
+    const bob = {
+        text: 'this is text',
+        img: 'this is img url',
+        created: new Date()
+    }
+
+    res.send(bob);
+});
+
 app.get('/', (req, res) =>{
 
     res.sendFile(__dirname+'/dist/index.html');
