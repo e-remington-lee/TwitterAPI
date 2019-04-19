607c1decb0c4dc53b0b01bf677f391d242363fe8
Erik Remington Lee <eriklee19@gmail.com>
2019-04-18 20:56:53 -0400

    added config to gitignore

--- node-server/config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    consumer_key: '',  
-    consumer_secret: '5GC3Q64Mi0LYdUwQNGZoGKVK6UzzWmyigVSFvjrVY4FP7jEmag',
-    access_token: '884287919002976256-rrOUAKw22CyxBtXv4vATJuagFbKGZIj',  
-    access_token_secret: 'KeIejPoA9jDHFt7hcf9sBFfeW9F0EZWR0g5AYSrWAO4wo'
-  }
