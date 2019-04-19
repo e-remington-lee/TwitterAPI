5da7e4afec18b268881a23802d9dab4a1928a19f
Erik Remington Lee <eriklee19@gmail.com>
2019-04-18 21:16:05 -0400

    served server on localhost

--- node-server/index.js
+++ node-server/index.js
@@ -5,9 +5,9 @@ var server = app.listen(3003, () => {
     console.log('server initialized')
 });
 
-app.use(express.static(__dirname+'/src'));
+app.use(express.static(__dirname+'/dist'));
 
 app.get('/', (req, res) =>{
-    console.log('dfa')
-    res.send("hello");
+
+    res.sendFile(__dirname+'/dist/index.html');
 });
