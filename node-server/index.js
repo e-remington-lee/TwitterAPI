4ecaa687d790dcace7999bf874d4d855d170ce70
Erik Remington Lee <eriklee19@gmail.com>
2019-04-23 22:29:23 -0400

    working with data.service

--- node-server/index.js
+++ node-server/index.js
@@ -3,8 +3,8 @@ const app = express();
 
 const access = require("./config.js");
 
-// const Twit = require('Twit');
-// const T = new Twit(access);
+const Twit = require('Twit');
+const T = new Twit(access);
 
 app.listen(3003, () => {
     console.log('server initialized on 3003')
@@ -14,14 +14,14 @@ app.listen(3003, () => {
 
 app.use(express.static(__dirname+'/dist'));
 
-app.get('/api/bob', (req, res) => {
-    const bob = {
-        text: 'this is text',
-        img: 'this is img url',
-        created: new Date()
-    }
-    res.send(bob);
-});
+// app.get('/api/bob', (req, res) => {
+//     const bob = {
+//         text: 'this is text',
+//         img: 'this is img url',
+//         created: new Date()
+//     }
+//     res.send(bob);
+// });
 
 
 app.get('/*', (req, res) =>{
@@ -31,3 +31,28 @@ app.get('/*', (req, res) =>{
 });
 
 
+var serachData2 = {
+    screen_name: 'elonmusk',
+    count: 10,
+    include_rts: false,
+    exclude_replies: false,
+    trim_user: true
+};
+
+T.get('statuses/user_timeline', serachData2, getData2);
+
+function getData2 (err,data,response) {
+
+    var userTweet = data;
+    let listTweets = [];
+ 
+    userTweet.forEach(function(item){
+        listTweets.push(item.text);
+    });
+
+    let length = listTweets.length;
+    // console.log(length+' elon tweets');
+    let randomSelection = Math.floor(Math.random()*(length+1));
+    // console.log('Elon Tweet: ' + listTweets[randomSelection]);
+    return listTweets[randomSelection]
+};
