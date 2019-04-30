317d32bda13670e343b42c3a1190e340f16c877c
Erik Remington Lee <eriklee19@gmail.com>
2019-04-30 18:41:59 -0400

    got hashtags working

--- angular/src/app/twitter-search/twitter-search.component.ts
+++ angular/src/app/twitter-search/twitter-search.component.ts
@@ -8,8 +8,6 @@ import { DataService } from "../data.service";
 })
 export class TwitterSearchComponent implements OnInit {
 
-  tweet1Text: String;
-  tweet1IMG: String;
   jsonTweet: Object;
   searchQuery: String;
 
@@ -24,8 +22,7 @@ export class TwitterSearchComponent implements OnInit {
     this.data.getSearch(this.searchQuery).subscribe(data => {
       this.jsonTweet = data;
       console.log(data)
-      this.tweet1Text = this.jsonTweet[0].text
-      this.tweet1IMG= this.jsonTweet[0].user.profile_image_url
+
 
     });
   };
