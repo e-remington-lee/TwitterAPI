36cfb60a2a7e4aefa147a3f8573c0a4afd8bca1f
Erik Remington Lee <eriklee19@gmail.com>
2019-04-30 20:06:30 -0400

    website styling completed

--- angular/src/app/random-tweet/random-tweet.component.ts
+++ angular/src/app/random-tweet/random-tweet.component.ts
@@ -11,6 +11,7 @@ export class RandomTweetComponent implements OnInit {
   randomTweet: Object;
   queryPerson: Object;
   person: String;
+  tweetImage: String;
   randomNumber: String;
 
   constructor(private data: DataService) { }
@@ -38,26 +39,31 @@ export class RandomTweetComponent implements OnInit {
     //images are 400x400
     this.person = 'Elon Musk Tweeted:';
     this.queryPerson ='elonmusk';
+    this.tweetImage ='https://i.imgur.com/CNhZzYY.jpg'
   }
 
   constance() {
     this.person = 'Constance Wu Tweeted:';
     this.queryPerson ='constancewu';
+    this.tweetImage ='https://speakerbookingagency.com/wp-content/uploads/bb-plugin/cache/constancewu-square.jpg'
   }
 
   gal() {
     this.person = 'Gal Gadot Tweeted:';
     this.queryPerson ='galgadot';
+    this.tweetImage = 'https://i.imgur.com/YxrCsT6.jpg'
   }
 
   daymond() {
     this.person = 'Daymond John Tweeted:';
     this.queryPerson ='thesharkdaymond';
+    this.tweetImage = 'https://pbs.twimg.com/profile_images/1048022980863954944/eZvGANn0_400x400.jpg'
   }
 
   ben() {
     this.person = 'Ben&Jerry\'s Tweeted:';
     this.queryPerson ='benandjerrys';
+    this.tweetImage = 'https://www.benjerry.com/files/live/sites/systemsite/files/whats-new/6588-flavor-flops/6-Hazed-Confused-400x400.jpg'
   }
 
 }
